2c309059d3ee33cf4c474b1fcffa946b4a1ef6e0
giacomo <youragequitforxd@gmail.com>
2024-08-05 12:49:44 +0200

    Fix reaction parser

--- chemistryIO/parsers/reaction_parser.py
+++ chemistryIO/parsers/reaction_parser.py
@@ -1,32 +1,26 @@
-from classes import CondReactionClass, CllReactionClass
-from utils.constants import REACTIONS_SECTION
+from classes import CondReactionClass, CllReactionClass, LengthClass
 
 def parse_reactions(line, data):
-    error_msg = f'Error parsing {REACTIONS_SECTION} section:'
-
-    parts = line.split()
+    parts = [part.replace("R-", "").replace("-R", "") for part in line.split()]
     if len(parts) != 3:
-        raise ValueError(f"{error_msg} condensation correct form:\n<reactant_1> <reactant_2> <reaction_speed>\nCleavage correct form:\n<reactant> <reaction_speed> <n_split> [<cleavage_point>]")
-    
-    reactant_1 = parts[0]
-    reactant_2 = parts[1] # can be a string or a number based on the reaction type
-    reaction_speed = parts[-1]
-
-    if reactant_1.startswith("R-") and reactant_2.endswith("-R"): # condensation reaction
-        try:
-            reaction_speed = float(reaction_speed)
-            data["reactions"]["conds"].append(CondReactionClass(reactant_1, reactant_2, reaction_speed))
-        except ValueError:
-            raise ValueError(f"{error_msg} reaction speed must be a float.")
-    elif reactant_1.startswith("R-") and reactant_1.endswith("-R"):
-        try:
+        raise ValueError(
+            "Error!\nCondensation correct form:\n<reactant_1> <reactant_2> <reaction_speed>\nCleavage correct form:\n<reactant> <reaction_speed> <n_split>"
+        )
+    try:
+        reaction_speed = float(parts[-1])
+        if parts[1].isdigit():
             n_split = int(parts[1])
-            reaction_speed = float(reaction_speed)
-            if n_split >= len(reactant_1):
-                raise RuntimeError(f"{error_msg} the value of the third parameter for cleavage reaction class must be less than the size of the defined string\nFor example R-AABBA-R, n_split must be < 5!")
-            data["reactions"]["clls"].append(CllReactionClass(reactant_1, n_split, reaction_speed))
-        except ValueError:
-            raise ValueError(f"{error_msg} n_split, and reaction speed must be integers and float respectively.")
-    else:
-        raise ValueError(f"{error_msg} invalid format for reaction '{line}'.")
+            if n_split >= len(parts[0]):
+                raise RuntimeError("Error!\nThe value of the third parameter for cleavage reaction class must be less than the size of the defined string\nFor example R-AABBA-R, n_split must be < 5!")
+            data["reactions"]["clls"].append(CllReactionClass(parts[0], n_split, reaction_speed))
+        else:
+            data["reactions"]["conds"].append(CondReactionClass(parts[0], parts[1], reaction_speed))
+    except ValueError:
+        raise ValueError("Error!\nReaction speed must be a float.")
 
+def parse_len_classes(line):
+    parts = line.split()
+    if len(parts) != 4:
+        raise ValueError("Error!\nLEN_CLASSES correct form:\n<class lengths> <p_cata_cond> <p_cata_cll> <specificity>")
+    classes = parts[0].replace('[', '').replace(']', '').split(',')
+    return [LengthClass(classes, parts[1], parts[2], parts[3])]
